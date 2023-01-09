6e42acd0b32cfc02bc76a38f3d0f9cedc526e65f
Daniel Upstone <zealvurte@users.noreply.github.com>
2023-01-09 17:49:38 +0000

    First accessibility pass
    Changes to HTML to support keyboard based skin controls, correct visibility & display changes, and aria attributes.
    Changes to CSS to support keyboard focus styles for skin controls, and appropriate visibility & display changes.
    Significant rewrite of JS for tooltip handling, aria attributes, and visibility & display changes.

--- scripts/owlls.js
+++ scripts/owlls.js
@@ -10,67 +10,138 @@ const isNumeric = function (value) {
 	//console.debug('isNumeric',value,'->',!isNaN(value))
 	return !isNaN(value)
 }
-const setElementData = function (element,property,value,type) {
-	const d = element.dataset
-	let pa = property, r
-	if (property.indexOf('-') === -1) {
-		pa = 'data-'+property.replace(/([a-z])([A-Z])/g,(m, g1, g2) => g1+'-'+g2).toLowerCase()
+const setElementAttribute = function (element,attribute,value,type) {
+	const a = attribute
+	let property, result
+	if (/([a-z])([A-Z])/.test(attribute)) {	// Property provided
+		property = attribute
+		attribute = attribute.replace(/\-([A-z])/g,(m, g1) => g1.toUpperCase())
 	}
-	else {
-		property = property.replace(/^data\-/,'').replace(/\-([A-z])/g,(m, g1) => g1.toUpperCase())
+	else {	// Attribute provided
+		property = attribute.replace(/([a-z])([A-Z])/g,(m, g1, g2) => g1+'-'+g2).toLowerCase()
 	}
 	if (value === undefined || (type === 'boolean' && (value === false || value === null)) || (type === 'number' && !isNumeric(value))) {
-		r = 'deleted'
-		delete d[property]
+		result = 'deleted'
+		delete element.removeAttribute(attribute)
 	}
 	else {
-		r = 'set'
-		d[property] = (type === 'boolean' && value === true) ? '' : (type === 'number' ? parseFloat(value) : value)
+		result = 'set'
+		element.setAttribute(attribute,(type === 'boolean' && value === true) ? '' : (type === 'number' ? parseFloat(value) : value))
 	}
-	if (type === undefined) {
-		//console.debug('setElementData',element,property,pa,value,'->',r,'->',getElementData(element,property),getElementData(element,pa))
-	}
-	else {
-		//console.debug('setElementData',element,property,pa,value,'('+type+')','->',r,'->',getElementData(element,property,type),getElementData(element,pa,type))
-	}
-	return d[property]
+	//console.debug('setElementAttribute',element,a,value,type,'->',attribute,property,result,'->',element.getAttribute(attribute),element[property],'->',getElementAttribute(element,attribute,type))
+	return getElementAttribute(element,attribute,type)
 }
-const getElementData = function (element,property,type) {
-	const d = element.dataset
-	let pa = property, v
-	if (property.indexOf('-') === -1) {
-		v = d[property]
-		pa = 'data-'+property.replace(/([a-z])([A-Z])/g,(m, g1, g2) => g1+'-'+g2).toLowerCase()
+const getElementAttribute = function (element,attribute,type) {
+	//const a = attribute
+	let property, value
+	if (/([a-z])([A-Z])/.test(attribute)) {	// Property provided
+		property = attribute
+		attribute = attribute.replace(/\-([A-z])/g,(m, g1) => g1.toUpperCase())
 	}
-	else {
-		property = property.replace(/^data\-/,'')
-		v = element.getAttribute('data-'+property)
-		property = property.replace(/\-([A-z])/g,(m, g1) => g1.toUpperCase())
+	else {	// Attribute provided
+		property = attribute.replace(/([a-z])([A-Z])/g,(m, g1, g2) => g1+'-'+g2).toLowerCase()
 	}
+	value = element.getAttribute(attribute)
 	if (type === 'boolean') {
-		v = v === '' && true || false
+		value = value === '' && true || false
 	}
 	else if (type === 'number') {
-		v = isNumeric(v) ? parseFloat(v) : undefined
+		value = isNumeric(value) ? parseFloat(value) : undefined
 	}
 	else {
-		v = v === 'null' ? null : (v === 'false' ? false : v)
+		value = value === 'null' ? null : (value === 'false' ? false : value)
 	}
-	return v
+	//console.debug('getElementAttribute',element,a,type,'->',attribute,property,'->',element.getAttribute(attribute),element[property],'->',value)
+	return value
 }
-const getElementScrolledPosition = function (element) {
-	const p = element.getBoundingClientRect()
-	return {
-		top: p.top + window.scrollY,
-		left: p.left + window.scrollX,
-		bottom: p.bottom + window.scrollY,
-		right: p.right + window.scrollX
+const setElementData = function (element,property,value,type) {
+	//const p = property
+	const data = element.dataset
+	let attribute, result
+	if (property.indexOf('-') !== -1) {	// Attribute provided
+		attribute = 'data-'+property.replace(/^data\-/,'')
+		property = property.replace(/^data\-/,'').replace(/\-([A-z])/g,(m, g1) => g1.toUpperCase())
+	}
+	else {
+		attribute = 'data-'+property.replace(/([a-z])([A-Z])/g,(m, g1, g2) => g1+'-'+g2).toLowerCase()
+	}
+	if (value === undefined || (type === 'boolean' && (value === false || value === null)) || (type === 'number' && !isNumeric(value))) {
+		result = 'deleted'
+		delete data[property]
+	}
+	else {
+		result = 'set'
+		data[property] = (type === 'boolean' && value === true) ? '' : (type === 'number' ? parseFloat(value) : value)
+	}
+	//console.debug('setElementData',element,p,value,type,'->',data,property,attribute,result,'->',data[property],element.getAttribute(attribute),'->',getElementData(element,property,type))
+	return getElementData(element,property,type)
+}
+const getElementData = function (element,property,type) {
+	//const p = property
+	const data = element.dataset
+	let attribute, value
+	if (property.indexOf('-') !== -1) {	// Attribute provided
+		attribute = 'data-'+property.replace(/^data\-/,'')
+		property = property.replace(/^data\-/,'').replace(/\-([A-z])/g,(m, g1) => g1.toUpperCase())
+	}
+	else {
+		attribute = 'data-'+property.replace(/([a-z])([A-Z])/g,(m, g1, g2) => g1+'-'+g2).toLowerCase()
+	}
+	value = data[property]
+	if (type === 'boolean') {
+		value = value === '' && true || false
+	}
+	else if (type === 'number') {
+		value = isNumeric(value) ? parseFloat(value) : undefined
+	}
+	else {
+		value = value === 'null' ? null : (value === 'false' ? false : value)
+	}
+	//console.debug('getElementData',element,p,type,'->',data,property,attribute,'->',data[property],element.getAttribute(attribute),'->',value)
+	return value
+}
+const getComputedStyleValue = function (element,pseudoElement,property) {
+	//const p = property
+	const computedStyle = getComputedStyle(element,pseudoElement)
+	let camelCaseProperty, value
+	if (property.indexOf('-') === -1) {	// Camel-case property provided
+		camelCaseProperty = property
+		property = property.replace(/([a-z])([A-Z])/g,(m, g1, g2) => g1+'-'+g2).toLowerCase()
+	}
+	else {	// Hyphenated property provided
+		camelCaseProperty = property.replace(/\-([A-z])/g,(m, g1) => g1.toUpperCase())
+	}
+	value = computedStyle[property]
+	//console.debug('getComputedStyleValue',element,pseudoElement,p,'->',computedStyle,property,camelCaseProperty,'->',computedStyle[property],computedStyle[camelCaseProperty],computedStyle.getPropertyValue(property),'->',value)
+	return value
+}
+const hideAfterEventHandler = function (event) {
+	const t = event.target
+	if (t === event.currentTarget) {
+		t.hidden = true
+		return true
+	}
+}
+const hideAfterEvent = function (element,events,handlerController) {
+	handlerController = handlerController || new AbortController();
+	(Array.isArray(events) ? events : [events]).forEach(e => {
+		element.addEventListener(e,(event) => hideAfterEventHandler(event) && handlerController.abort(),{
+			'signal': handlerController.signal
+		})
+	})
+}
+const setElementVisibility = function (element,show,events,handlerController) {
+	if (getElementData(element,'hidden','boolean') === show) {
+		if (!show) {
+			hideAfterEvent(element,events || ['animationend','animationcancel'],handlerController)
+		}
+		else {
+			element.hidden = !show
+		}
+		setElementData(element,'hidden',!show,'boolean')
 	}
 }
 var overflowContainerPattern = /^(?!visible).*$/
-const getComputedStyleValue = function (element,pseudoElement,property) {
-	return getComputedStyle(element,pseudoElement)[property]
-}
 const isOverflowContainer = function (element) {
 	return overflowContainerPattern.test(getComputedStyleValue(element,null,'overflow-x')) || overflowContainerPattern.test(getComputedStyleValue(element,null,'overflow-y'))
 }
@@ -82,10 +153,9 @@ const getOverflowContainer = function (element) {
 const tooltip = document.getElementById('tooltip')
 const clampTooltipPosition = function () {
 	tooltip.style.transform = ''
-	const overflowContainer = getOverflowContainer(tooltip.parentNode)
-	const overflowContainerPosition = getElementScrolledPosition(overflowContainer)
+	const overflowContainerPosition = getOverflowContainer(tooltip.parentNode).getBoundingClientRect()
 	// Horizontal positioning (left is anchored to offsetParent's left)
-	let tooltipPosition = getElementScrolledPosition(tooltip)
+	let tooltipPosition = tooltip.getBoundingClientRect()
 	let updatePosition = false
 	let translateX = 0
 	let leftDiff = tooltipPosition.left - overflowContainerPosition.left
@@ -103,14 +173,14 @@ const clampTooltipPosition = function () {
 		}
 	}
 	// Vertical positioning (top is anchored to offsetParent's bottom)
-	tooltipPosition = updatePosition ? getElementScrolledPosition(tooltip) : tooltipPosition
+	tooltipPosition = updatePosition ? tooltip.getBoundingClientRect() : tooltipPosition
 	let translateY = 0
 	let topDiff = tooltipPosition.top - overflowContainerPosition.top
 	let bottomDiff = tooltipPosition.bottom - overflowContainerPosition.bottom
 	if (bottomDiff > 0) {
 		if (topDiff > 0) {
-			// Anchor bottom to offsetParent's top via the top difference
-			const offsetParentPosition = getElementScrolledPosition(tooltip.offsetParent)
+			// Anchor bottom to offsetParent's top via the top difference (assumes offsetParent is the owner element, and it's much smaller than the overflowContainer)
+			const offsetParentPosition = tooltip.offsetParent.getBoundingClientRect()
 			translateY -= tooltipPosition.bottom - offsetParentPosition.top
 			bottomDiff -= translateY
 			// If it still overflows, then the overflowContainer is too small, or the tooltip too big, so change one of those instead
@@ -118,52 +188,67 @@ const clampTooltipPosition = function () {
 	}
 	tooltip.style.transform = `translate(${translateX}px,${translateY}px)`
 }
-const doAction = function (event) {
-	if (!getSelection().isCollapsed) { return }
+let tooltipVisibilityHandlerController = new AbortController()
+const resetTooltipVisibilityHandlerController = function () {
+	tooltipVisibilityHandlerController.abort()	// Abort any pending visibility changes to hidden
+	tooltipVisibilityHandlerController = new AbortController()
+	return tooltipVisibilityHandlerController
+}
+const setTooltip = function (event) {
 	const ct = event.currentTarget
-	const actions = getElementData(ct,'action','string').split(';')
-	const targets = (getElementData(ct,'target','string') || '').split(';')
-	const targetIgnores = (getElementData(ct,'target-ignore','string') || '').split(';')
+	setElementAttribute(tooltip.parentElement,'aria-describedby','','string')
+	setElementVisibility(tooltip,false,null,resetTooltipVisibilityHandlerController())
+	if (!ct.contains(tooltip)) {
+		ct.append(tooltip)
+		tooltip.innerHTML = `<p>${getElementData(ct,'tooltip','string')}</p>`
+		tooltip.hidden = false	// Must be displayed for clamp calculations
+		clampTooltipPosition()
+	}
+	setElementVisibility(tooltip,true,null,resetTooltipVisibilityHandlerController())
+	setElementAttribute(ct,'aria-describedby','tooltip','string')
+}
+const unsetTooltip = function (event) {
+	setElementAttribute(tooltip.parentElement,'aria-describedby','','string')
+	setElementVisibility(tooltip,false,null,resetTooltipVisibilityHandlerController())
+}
+document.addEventListener('keydown',(event) => {
+	if (event.key === 'Escape') {
+		unsetTooltip(event)
+	}
+})
+const doAction = function (event,actionType) {
+	const ct = event.currentTarget
+	actionType = actionType ? actionType+'-' : '';
+	const actions = getElementData(ct,actionType+'action','string').split(';')
+	const targets = (getElementData(ct,actionType+'target','string') || '').split(';')
+	const targetIgnores = (getElementData(ct,actionType+'target-ignore','string') || '').split(';')
 	actions.forEach((action,i) => {
 		const args = action.split('.')
 		let target = (!targets[i] || targets[i] === 'this') ? [ct] : document.querySelectorAll(targets[i])
 		const targetIgnore = targetIgnores[i] ? ((targetIgnores[i] === 'this') ? [ct] : document.querySelectorAll(targetIgnores[i])) : null
 		target = targetIgnore ? [...target].filter(t => ![...targetIgnore].reduce((ignore,ti) => ignore ||  t === ti,false)) : target
 		switch (args[0]) {
-			case 'state':
+			case 'visibility':
 				if (args[1] === 'toggle') {
-					target.forEach(t => setElementData(t,args[0],getElementData(t,args[0],'string') === 'active' ? 'inert' : 'active','string'))
+					target.forEach(t => setElementVisibility(t,getElementData(element,'hidden','boolean')))
 				}
-				else if (args[1] === 'active' || args[1] === 'inert') {
-					target.forEach(t => setElementData(t,args[0],args[1],'string'))
+				else if (args[1] === 'show' || args[1] === 'hide') {
+					target.forEach(t => setElementVisibility(t,args[1] === 'show'))
 				}
 				break
 			case 'expandable':
-				if (args[1] === 'toggle') {
-					target.forEach(t => setElementData(t,args[0],getElementData(t,'expanded','boolean') ? false : true,'boolean'))
-				}
-				else if (args[1] === 'expand' || args[1] === 'collapse') {
-					target.forEach(t => setElementData(t,'expanded',args[1] === 'expand' ? true : false,'boolean'))
+				args[1] = args[1] === 'toggle' ? (getElementAttribute(ct,'aria-expanded','string') === 'true' ? 'collapse' : 'expand') : args[1]
+				if (args[1] === 'expand' || args[1] === 'collapse') {
+					const expand = (args[1] === 'expand').toString()
+					target.forEach(t => {
+						setElementAttribute(t,'aria-expanded',expand,'string')
+						document.querySelectorAll(`[aria-controls="${t.id}"]`).forEach(c => {
+							setElementAttribute(c,'aria-expanded',expand,'string')
+						})
+					})
+					setElementAttribute(ct,'aria-expanded',expand,'string')
 				}
 				break
-			case 'tooltip':
-				const sameTooltip = ct.contains(tooltip)
-				const state = getElementData(tooltip,'state','string')
-				args[1] = args[1] === 'toggle' ? ((sameTooltip && state === 'active') ? 'hide' : 'show') : args[1]
-				if (args[1] === 'show') {
-					if (state === 'active') {
-						setElementData(tooltip,'state','inert','string')
-					}
-					if (!sameTooltip) {
-						ct.append(tooltip)
-						tooltip.innerHTML = [...target].reduce((content,t) => content+`<p>${t.title}</p>`,'')
-						clampTooltipPosition()
-					}
-					setElementData(tooltip,'state','active','string')
-				}
-				else if (args[1] === 'hide' && state === 'active') {
-					setElementData(tooltip,'state','inert','string')
-				}
 			case 'scrollTo':
 				args[1] = (args[1] && (typeof(args[1]) === 'number' || /^[1-9]\d*(?:\.\d+)?%$/.test(args[1]) || /^(start|center|end|top|middle|bottom)$/.test(args[1]))) ? args[1] : 0
 				args[2] = (args[2] && (typeof(args[2]) === 'number' || /^[1-9]\d*(?:\.\d+)?%$/.test(args[2]) || /^(start|center|end|left|middle|right)$/.test(args[2]))) ? args[2] : ( /^(start|center|end)$/.test(args[1]) ? args[1] : 0)
@@ -184,43 +269,29 @@ const doAction = function (event) {
 		}
 	})
 }
-const hideAfterEventHandler = function (event) {
-	const t = event.target
-	if (t === event.currentTarget) {
-		t.hidden = true
-		return true
-	}
+const doClickAction = function (event) {
+	const selection = getSelection()
+	if (!selection.isCollapsed && selection.containsNode(event.currentTarget,true)) { return }
+	return doAction(event)
 }
-const hideAfterEvent = function (element,events) {
-	const handlerController = new AbortController();
-	(Array.isArray(events) ? events : [events]).forEach(e => {
-		element.addEventListener(e,(event) => hideAfterEventHandler(event) && handlerController.abort(),{
-			'signal': handlerController.signal
-		})
-	})
+const doFocusAction = function (event) {
+	return doAction(event,'focus')
+}
+const doUnfocusAction = function (event) {
+	return doAction(event,'unfocus')
 }
 const isCurrentlyAvailable = function (start,end) {
 	const now = Date.now()
 	return (start ? new Date(start) <= now : true) && (end ? new Date(end) >= now : true)
 }
 const filtersChanged = function (event) {
-	const noSkinsState = [...skinArticles].filter(filterSkins).length === 0 ? 'active' : 'inert'
-	if (getElementData(noSkins,'state','string') !== noSkinsState) {
-		setElementData(noSkins,'state',noSkinsState,'string')
-	}
+	const show = [...skinArticles].filter(filterSkins).length === 0 ? true : false
+	setElementVisibility(noSkins,show)
 }
 const filterSkins = function (skin) {
-	const newState = ((navAvailabilityCheckbox.checked && !getElementData(skin,'available','boolean')) || (navHeroSelect.value !== '' && navHeroSelect.value !== getElementData(skin,'hero','string')) || (navSearch.value !== '' && getElementData(skin,'name','string').toLowerCase().indexOf(navSearch.value.toLowerCase()) < 0)) ? 'inert' : 'active'
-	if (getElementData(skin,'state','string') !== newState) {
-		if (newState === 'inert') {
-			hideAfterEvent(skin,['animationend','animationcancel'])
-		}
-		else {
-			skin.hidden = false
-		}
-		setElementData(skin,'state',newState,'string')
-	}
-	return newState === 'active'
+	const show = !((navAvailabilityCheckbox.checked && !getElementData(skin,'available','boolean')) || (navHeroSelect.value !== '' && navHeroSelect.value !== getElementData(skin,'hero','string')) || (navSearch.value !== '' && getElementData(skin,'name','string').toLowerCase().indexOf(navSearch.value.toLowerCase()) < 0))
+	setElementVisibility(skin,show)
+	return show
 }
 
 // Set element references
@@ -248,4 +319,12 @@ navSearch.addEventListener('input',filtersChanged)
 filtersChanged()
 
 // Hookup actions
-document.querySelectorAll(`[data-action]`).forEach(e => e.addEventListener('click',doAction))
+document.querySelectorAll('[data-action]').forEach(e => e.addEventListener('click',doClickAction))
+
+// Hookup tooltips
+document.querySelectorAll('[data-tooltip]').forEach(e => {
+	e.addEventListener('focus',setTooltip)
+	e.addEventListener('mouseenter',setTooltip)
+	e.addEventListener('blur',unsetTooltip)
+	e.addEventListener('mouseleave',unsetTooltip)
+})
