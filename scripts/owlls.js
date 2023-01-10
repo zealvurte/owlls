82aba2afedf598d03a72e5b6a38d7648cc49bae2
Daniel Upstone <zealvurte@users.noreply.github.com>
2023-01-10 15:14:12 +0000

    Skin actions
    Restructured JS and added higher level skin actions.
    HTML now uses skin actions instead of individual lower level ones.

--- scripts/owlls.js
+++ scripts/owlls.js
@@ -1,3 +1,4 @@
+// Helpers
 const isNumeric = function (value) {
 	if (typeof(value) === 'number' || typeof(value) === 'bigint' || (typeof(value) === 'string' && value == 0)) {
 		//console.debug('isNumeric',value,'->',true,'fast')
@@ -10,6 +11,8 @@ const isNumeric = function (value) {
 	//console.debug('isNumeric',value,'->',!isNaN(value))
 	return !isNaN(value)
 }
+
+// Element attributes
 const setElementAttribute = function (element,attribute,value,type) {
 	const a = attribute
 	let property, result
@@ -54,6 +57,8 @@ const getElementAttribute = function (element,attribute,type) {
 	//console.debug('getElementAttribute',element,a,type,'->',attribute,property,'->',element.getAttribute(attribute),element[property],'->',value)
 	return value
 }
+
+// Element data
 const setElementData = function (element,property,value,type) {
 	//const p = property
 	const data = element.dataset
@@ -100,6 +105,8 @@ const getElementData = function (element,property,type) {
 	//console.debug('getElementData',element,p,type,'->',data,property,attribute,'->',data[property],element.getAttribute(attribute),'->',value)
 	return value
 }
+
+// Element computed styles
 const getComputedStyleValue = function (element,pseudoElement,property) {
 	//const p = property
 	const computedStyle = getComputedStyle(element,pseudoElement)
@@ -115,6 +122,19 @@ const getComputedStyleValue = function (element,pseudoElement,property) {
 	//console.debug('getComputedStyleValue',element,pseudoElement,p,'->',computedStyle,property,camelCaseProperty,'->',computedStyle[property],computedStyle[camelCaseProperty],computedStyle.getPropertyValue(property),'->',value)
 	return value
 }
+
+// Overflow containers
+var overflowContainerPattern = /^(?!visible).*$/
+const isOverflowContainer = function (element) {
+	return overflowContainerPattern.test(getComputedStyleValue(element,null,'overflow-x')) || overflowContainerPattern.test(getComputedStyleValue(element,null,'overflow-y'))
+}
+const getOverflowContainer = function (element) {
+	if (!(element instanceof HTMLElement)) { return document.body }
+	if (isOverflowContainer(element)) { return element }
+	else { return getOverflowContainer(element.parentNode) }
+}
+
+// Element visibility
 const hideAfterEventHandler = function (event) {
 	const t = event.target
 	if (t === event.currentTarget) {
@@ -141,15 +161,129 @@ const setElementVisibility = function (element,show,events,handlerController) {
 		setElementData(element,'hidden',!show,'boolean')
 	}
 }
-var overflowContainerPattern = /^(?!visible).*$/
-const isOverflowContainer = function (element) {
-	return overflowContainerPattern.test(getComputedStyleValue(element,null,'overflow-x')) || overflowContainerPattern.test(getComputedStyleValue(element,null,'overflow-y'))
+const toggleElementVisibility = function (element,show,events,handlerController) {
+	setElementVisibility(element,getElementData(element,'hidden','boolean'),events,handlerController)
 }
-const getOverflowContainer = function (element) {
-	if (!(element instanceof HTMLElement)) { return document.body }
-	if (isOverflowContainer(element)) { return element }
-	else { return getOverflowContainer(element.parentNode) }
+
+// Element Expandability
+const setElementExpandability = function (element,expand) {
+	setElementAttribute(element,'aria-expanded',expand,'string')
+	document.querySelectorAll(`[aria-controls="${expand.id}"]`).forEach(c => {
+		setElementAttribute(c,'aria-expanded',expand,'string')
+	})
 }
+const toggleElementExpandability = function (element) {
+	setElementExpandability(element,getElementAttribute(element,'aria-expanded','string') !== 'true')
+}
+
+// Scroll Element To
+const scrollElementTo = function (element,top,left,behaviour) {
+	top = (top && (typeof(top) === 'number' || /^[1-9]\d*(?:\.\d+)?%$/.test(top) || /^(start|center|end|top|middle|bottom)$/.test(top))) ? top : 0
+	left = (left && (typeof(left) === 'number' || /^[1-9]\d*(?:\.\d+)?%$/.test(left) || /^(start|center|end|left|middle|right)$/.test(left))) ? left : ( /^(start|center|end)$/.test(top) ? top : 0)
+	element.scroll({
+		'top': typeof(top) === 'number' ? top : ((top === 'end' || top === 'bottom') ? t.scrollHeight : ((top === 'center' || top === 'middle') ? Math.round(t.scrollHeight/2) : (/^[1-9]\d*(?:\.\d+)?%$/.test(top) ? Math.round(parseInt(/^([1-9]\d*(?:\.\d+)?)%$/.match(top)[1])/100*t.scrollHeight) : 0 ))),
+		'left': typeof(left) === 'number' ? left : ((left === 'end' || left === 'right') ? t.scrollWidth : ((left === 'center' || left === 'middle') ? Math.round(t.scrollWidth/2) : (/^[1-9]\d*(?:\.\d+)?%$/.test(left) ? Math.round(parseInt(/^([1-9]\d*(?:\.\d+)?)%$/.match(left)[1])/100*t.scrollWidth) : 0))),
+		'behaviour': behaviour === 'auto' ? behaviour : 'smooth'
+	})
+}
+
+// Scroll To Element
+const scrollToElement = function (element,block,inline,behaviour) {
+	block = (block && (block === 'start' || block === 'center' || block === 'end')) ? block : 'nearest'
+	element.scrollIntoView({
+		'block': block,
+		'inline': (inline && (inline === 'start' || inline === 'center' || inline === 'end')) ? inline : block,
+		'behaviour': behaviour === 'auto' ? behaviour : 'smooth'
+	})
+}
+
+// Actions
+const doAction = function (event,actionType) {
+	const ct = event.currentTarget
+	actionType = actionType ? actionType+'-' : '';
+	const actions = getElementData(ct,actionType+'action','string').split(';')
+	const targets = (getElementData(ct,actionType+'target','string') || '').split(';')
+	const targetIgnores = (getElementData(ct,actionType+'target-ignore','string') || '').split(';')
+	actions.forEach((action,i) => {
+		const args = action.split('.')
+		let target = (!targets[i] || targets[i] === 'this') ? [ct] : document.querySelectorAll(targets[i])
+		const targetIgnore = targetIgnores[i] ? ((targetIgnores[i] === 'this') ? [ct] : document.querySelectorAll(targetIgnores[i])) : null
+		target = targetIgnore ? [...target].filter(t => ![...targetIgnore].reduce((ignore,ti) => ignore ||  t === ti,false)) : target
+		switch (args[0]) {
+			case 'visibility':
+				if (args[1] === 'toggle') {
+					target.forEach(t => toggleElementVisibility(t))
+				}
+				else if (args[1] === 'show' || args[1] === 'hide') {
+					target.forEach(t => setElementVisibility(t,args[1] === 'show'))
+				}
+				break
+			case 'expandability':
+				if (args[1] === 'toggle') {
+					target.forEach(t => toggleElementExpandability(t))
+				}
+				if (args[1] === 'expand' || args[1] === 'collapse') {
+					target.forEach(t => setElementExpandability(t,args[1] === 'expand'))
+				}
+				break
+			case 'scrollTo':
+				target.forEach(t => scollElementTo(args[1],args[2],args[3]))
+				break
+			case 'scrollIntoView':
+				target.forEach(t => scollToElement(args[1],args[2],args[3]))
+				break
+			case 'skin':
+				if (args[1] === 'expandability' || args[1] === 'expand' || args[1] === 'collapse') {
+					args[2] = args[1] !== 'expandability' ? args[1] : args[2]
+					if (args[2] === 'toggle') {
+						target.forEach(t => {
+							toggleSkinExpandability(t)
+						})
+					}
+					else if (args[2] === 'expand' || args[2] === 'collapse') {
+						target.forEach(t => {
+							setSkinExpandability(t,args[2] === 'expand')
+						})
+					}
+				}
+				else if (args[1] === 'visibility' || args[1] === 'show' || args[1] === 'hide') {
+					args[2] = args[1] !== 'visibility' ? args[1] : args[2]
+					if (args[2] === 'toggle') {
+						target.forEach(t => {
+							toggleSkinVisibility(t)
+						})
+					}
+					else if (args[2] === 'show' || args[2] === 'hide') {
+						target.forEach(t => {
+							setSkinVisibility(t,args[2] === 'show')
+						})
+					}
+				}
+		}
+	})
+}
+const doClickAction = function (event) {
+	const selection = getSelection()
+	if (!selection.isCollapsed && selection.containsNode(event.currentTarget,true)) { return }	// Don't do click action if selecting inside event.currentTarget
+	return doAction(event)
+}
+document.querySelectorAll('[data-action]').forEach(e => e.addEventListener('click',doClickAction))
+const doFocusAction = function (event) {
+	return doAction(event,'focus')
+}
+document.querySelectorAll('[data-focus-action]').forEach(e => {
+	e.addEventListener('focus',doFocusAction)
+	e.addEventListener('mouseenter',doFocusAction)
+})
+const doUnfocusAction = function (event) {
+	return doAction(event,'unfocus')
+}
+document.querySelectorAll('[data-unfocus-action]').forEach(e => {
+	e.addEventListener('blur',doUnfocusAction)
+	e.addEventListener('mouseleave',doUnfocusAction)
+})
+
+// Tooltips
 const tooltip = document.getElementById('tooltip')
 const clampTooltipPosition = function () {
 	tooltip.style.transform = ''
@@ -216,115 +350,65 @@ document.addEventListener('keydown',(event) => {
 		unsetTooltip(event)
 	}
 })
-const doAction = function (event,actionType) {
-	const ct = event.currentTarget
-	actionType = actionType ? actionType+'-' : '';
-	const actions = getElementData(ct,actionType+'action','string').split(';')
-	const targets = (getElementData(ct,actionType+'target','string') || '').split(';')
-	const targetIgnores = (getElementData(ct,actionType+'target-ignore','string') || '').split(';')
-	actions.forEach((action,i) => {
-		const args = action.split('.')
-		let target = (!targets[i] || targets[i] === 'this') ? [ct] : document.querySelectorAll(targets[i])
-		const targetIgnore = targetIgnores[i] ? ((targetIgnores[i] === 'this') ? [ct] : document.querySelectorAll(targetIgnores[i])) : null
-		target = targetIgnore ? [...target].filter(t => ![...targetIgnore].reduce((ignore,ti) => ignore ||  t === ti,false)) : target
-		switch (args[0]) {
-			case 'visibility':
-				if (args[1] === 'toggle') {
-					target.forEach(t => setElementVisibility(t,getElementData(element,'hidden','boolean')))
-				}
-				else if (args[1] === 'show' || args[1] === 'hide') {
-					target.forEach(t => setElementVisibility(t,args[1] === 'show'))
-				}
-				break
-			case 'expandable':
-				args[1] = args[1] === 'toggle' ? (getElementAttribute(ct,'aria-expanded','string') === 'true' ? 'collapse' : 'expand') : args[1]
-				if (args[1] === 'expand' || args[1] === 'collapse') {
-					const expand = (args[1] === 'expand').toString()
-					target.forEach(t => {
-						setElementAttribute(t,'aria-expanded',expand,'string')
-						document.querySelectorAll(`[aria-controls="${t.id}"]`).forEach(c => {
-							setElementAttribute(c,'aria-expanded',expand,'string')
-						})
-					})
-					setElementAttribute(ct,'aria-expanded',expand,'string')
-				}
-				break
-			case 'scrollTo':
-				args[1] = (args[1] && (typeof(args[1]) === 'number' || /^[1-9]\d*(?:\.\d+)?%$/.test(args[1]) || /^(start|center|end|top|middle|bottom)$/.test(args[1]))) ? args[1] : 0
-				args[2] = (args[2] && (typeof(args[2]) === 'number' || /^[1-9]\d*(?:\.\d+)?%$/.test(args[2]) || /^(start|center|end|left|middle|right)$/.test(args[2]))) ? args[2] : ( /^(start|center|end)$/.test(args[1]) ? args[1] : 0)
-				target.forEach(t => t.scroll({
-					'top': typeof(args[1]) === 'number' ? args[1] : ((args[1] === 'end' || args[1] === 'bottom') ? t.scrollHeight : ((args[1] === 'center' || args[1] === 'middle') ? Math.round(t.scrollHeight/2) : (/^[1-9]\d*(?:\.\d+)?%$/.test(args[1]) ? Math.round(parseInt(/^([1-9]\d*(?:\.\d+)?)%$/.match(args[1])[1])/100*t.scrollHeight) : 0 ))),
-					'left': typeof(args[2]) === 'number' ? args[2] : ((args[2] === 'end' || args[2] === 'right') ? t.scrollWidth : ((args[2] === 'center' || args[2] === 'middle') ? Math.round(t.scrollWidth/2) : (/^[1-9]\d*(?:\.\d+)?%$/.test(args[2]) ? Math.round(parseInt(/^([1-9]\d*(?:\.\d+)?)%$/.match(args[2])[1])/100*t.scrollWidth) : 0))),
-					'behaviour': args[3] === 'auto' ? args[3] : 'smooth'
-				}))
-				break
-			case 'scrollIntoView':
-				args[1] = (args[1] && (args[1] === 'start' || args[1] === 'center' || args[1] === 'end')) ? args[1] : 'nearest'
-				target.forEach(t => t.scrollIntoView({
-					'block': args[1],
-					'inline': (args[2] && (args[2] === 'start' || args[2] === 'center' || args[2] === 'end')) ? args[2] : args[1],
-					'behaviour': args[3] === 'auto' ? args[3] : 'smooth'
-				}))
-				break
-		}
-	})
-}
-const doClickAction = function (event) {
-	const selection = getSelection()
-	if (!selection.isCollapsed && selection.containsNode(event.currentTarget,true)) { return }
-	return doAction(event)
-}
-const doFocusAction = function (event) {
-	return doAction(event,'focus')
-}
-const doUnfocusAction = function (event) {
-	return doAction(event,'unfocus')
-}
-const isCurrentlyAvailable = function (start,end) {
-	const now = Date.now()
-	return (start ? new Date(start) <= now : true) && (end ? new Date(end) >= now : true)
-}
-const filtersChanged = function (event) {
-	const show = [...skinArticles].filter(filterSkins).length === 0 ? true : false
-	setElementVisibility(noSkins,show)
-}
-const filterSkins = function (skin) {
-	const show = !((navAvailabilityCheckbox.checked && !getElementData(skin,'available','boolean')) || (navHeroSelect.value !== '' && navHeroSelect.value !== getElementData(skin,'hero','string')) || (navSearch.value !== '' && getElementData(skin,'name','string').toLowerCase().indexOf(navSearch.value.toLowerCase()) < 0))
-	setElementVisibility(skin,show)
-	return show
-}
-
-// Set element references
-const navHeroSelect = document.getElementById('nav_hero')
-const navSearch = document.getElementById('nav_search')
-const navSearchDatalist = document.getElementById('nav_search-data')
-const navAvailabilityCheckbox = document.getElementById('nav_availability')
-const contentMain = document.getElementById('content')
-const noSkins = document.getElementById('no-skins')
-const skinArticles = document.querySelectorAll('article.skin')
-
-// Process skin availability
-skinArticles.forEach(s => {
-	const available = isCurrentlyAvailable(getElementData(s,'availability-start','string'),getElementData(s,'availability-end','string'))
-	setElementData(s,'available',available,'boolean')
-	if (!available) {
-		s.querySelectorAll('section.skin-info main div.group:last-child :is(dt,dd):not(.historic)').forEach(hp => hp.classList.add('historic'))
-	}
-})
-
-// Hookup filtering & searching
-const navFilters = [navHeroSelect,navAvailabilityCheckbox]
-navFilters.forEach(f => f.addEventListener('change',filtersChanged))
-navSearch.addEventListener('input',filtersChanged)
-filtersChanged()
-
-// Hookup actions
-document.querySelectorAll('[data-action]').forEach(e => e.addEventListener('click',doClickAction))
-
-// Hookup tooltips
 document.querySelectorAll('[data-tooltip]').forEach(e => {
 	e.addEventListener('focus',setTooltip)
 	e.addEventListener('mouseenter',setTooltip)
 	e.addEventListener('blur',unsetTooltip)
 	e.addEventListener('mouseleave',unsetTooltip)
 })
+
+// Skins
+const skinArticles = document.querySelectorAll('article.skin')
+const setSkinVisibility = function (skin,show) {
+	const skinInfo = document.getElementById(`${skin.id}_info`)
+	if (!show) {
+		setSkinExpandability(skin,false)
+	}
+	setElementVisibility(skin,show)
+}
+const toggleSkinVisibility = function (skin) {
+	setSkinVisiblity(skin,getElementData(skin,'hidden','boolean'))
+}
+const setSkinExpandability = function (skin,expand) {
+	const skinInfo = document.getElementById(`${skin.id}_info`)
+	setElementVisibility(skinInfo,expand)
+	if (expand) {
+		scrollElementTo(skinInfo,'start')
+	}
+	setElementExpandability(skin,expand)
+}
+const toogleSkinExpandability = function (skin) {
+	setSkinExpandability(skin,getElementAttribute(skin,'aria-expanded','string') !== 'true')
+}
+const isSkinCurrentlyAvailable = function (start,end) {
+	const now = Date.now()
+	return (start ? new Date(start) <= now : true) && (end ? new Date(end) >= now : true)
+}
+skinArticles.forEach(s => {
+	const available = isSkinCurrentlyAvailable(getElementData(s,'availability-start','string'),getElementData(s,'availability-end','string'))
+	setElementData(s,'available',available,'boolean')
+	if (!available) {
+		s.querySelectorAll('section.skin-info main div.group:last-child :is(dt,dd):not(.historic)').forEach(hp => hp.classList.add('historic'))
+	}
+})
+
+// Filters
+const navHeroSelect = document.getElementById('nav_hero')
+const navSearch = document.getElementById('nav_search')
+const navSearchDatalist = document.getElementById('nav_search-data')
+const navAvailabilityCheckbox = document.getElementById('nav_availability')
+const contentMain = document.getElementById('content')
+const noSkins = document.getElementById('no-skins')
+const filtersChanged = function (event) {
+	const show = [...skinArticles].filter(filterSkins).length === 0 ? true : false
+	setElementVisibility(noSkins,show)
+}
+const filterSkins = function (skin) {
+	const show = !((navAvailabilityCheckbox.checked && !getElementData(skin,'available','boolean')) || (navHeroSelect.value !== '' && navHeroSelect.value !== getElementData(skin,'hero','string')) || (navSearch.value !== '' && getElementData(skin,'name','string').toLowerCase().indexOf(navSearch.value.toLowerCase()) < 0))
+	setSkinVisibility(skin,show)
+	return show
+}
+const navFilters = [navHeroSelect,navAvailabilityCheckbox]
+navFilters.forEach(f => f.addEventListener('change',filtersChanged))
+navSearch.addEventListener('input',filtersChanged)
+filtersChanged()
